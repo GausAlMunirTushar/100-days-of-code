1e2ff0b44b9115b6a8d6d7dce68c551ad5571a87
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2023-09-30 01:57:02 +0600

    login functinality completed

---
+++ deposit-withdraw/js/index.js
@@ -0,0 +1,19 @@
+
+// login into dashboard
+function login() {
+    let loginBtn = document.querySelector('#login')
+
+    loginBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    // check form email and password
+    if(email == 'gausalmunirtushar@gmail.com' && password == '1234'){
+        window.location.href = 'dashboard.html'
+    }
+    else{
+        alert('Your Email and Password invalid')
+    }
+})
+} login()
